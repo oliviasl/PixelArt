0e04f42263542c06945a559aef6a1ffca1964068
Stephen Lai <stephenlai@Stephens-MacBook.local>
2020-03-08 13:24:38 -0700

    files updated

--- Screen.java
+++ Screen.java
@@ -5,39 +5,109 @@ import java.awt.Font;
 import javax.swing.JPanel;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.Graphics2D;
 
 
-public class Screen extends JPanel implements MouseListener, ActionListener{
+public class Screen extends JPanel implements MouseListener, ActionListener, MouseMotionListener{
 	
 	
-	private Square[][] grid;
+	private Square[][] grid, undoGrid1, undoGrid2, undoGrid3;
 	private int redSet, greenSet, blueSet;
+	private boolean store;
+	private JButton clearButton, undoButton, fillButton, saveFileButton;
+	private JButton drawButton, eyeDropperButton;
+	private Font font1;
+	private String currentTool;
 	
 	
 	public Screen(){
 		
 		addMouseListener(this);
+		addMouseMotionListener(this);
+		setLayout(null);
 		
-		//grid
+		//grids
 		grid = new Square[16][16];
 		for(int r = 0;r < grid.length;r ++){
 			for(int c = 0;c < grid[r].length;c ++){
 				grid[r][c] = new Square();
 			}
 		}
+		undoGrid1 = new Square[16][16];
+		for(int r = 0;r < undoGrid1.length;r ++){
+			for(int c = 0;c < undoGrid1[r].length;c ++){
+				undoGrid1[r][c] = new Square();
+			}
+		}
+		undoGrid2 = new Square[16][16];
+		for(int r = 0;r < undoGrid2.length;r ++){
+			for(int c = 0;c < undoGrid2[r].length;c ++){
+				undoGrid2[r][c] = new Square();
+			}
+		}
+		undoGrid3 = new Square[16][16];
+		for(int r = 0;r < undoGrid3.length;r ++){
+			for(int c = 0;c < undoGrid3[r].length;c ++){
+				undoGrid3[r][c] = new Square();
+			}
+		}
 		
-		//color ints
+		//logistics
 		redSet = 0;
 		greenSet = 0;
 		blueSet = 0;
+		store = true;
+		currentTool = "Draw";
+		
+		
+		//font
+		font1 = new Font("Calibri",Font.PLAIN,15);
+		
+		//JButton
+		clearButton = new JButton("Clear Canvas");
+		clearButton.setBounds(10,500,150,30);
+		add(clearButton);
+		clearButton.addActionListener(this);
+		
+		undoButton = new JButton("Undo");
+		undoButton.setBounds(342,500,150,30);
+		add(undoButton);
+		undoButton.addActionListener(this);
+		
+		fillButton = new JButton("Fill");
+		fillButton.setBounds(570,360,150,30);
+		add(fillButton);
+		fillButton.addActionListener(this);
+		
+		saveFileButton = new JButton("Save File");
+		saveFileButton.setBounds(775,430,150,30);
+		add(saveFileButton);
+		saveFileButton.addActionListener(this);
+		
+		drawButton = new JButton("Draw");
+		drawButton.setBounds(570,395,150,30);
+		add(drawButton);
+		drawButton.addActionListener(this);
+		
+		eyeDropperButton = new JButton("Eye Dropper");
+		eyeDropperButton.setBounds(570,430,150,30);
+		add(eyeDropperButton);
+		eyeDropperButton.addActionListener(this);
 		
 	}
 	
 	
 	public Dimension getPreferredSize(){
-		return new Dimension(1000,700);
+		return new Dimension(980,575);
 		
 	}
 	
@@ -58,8 +128,8 @@ public class Screen extends JPanel implements MouseListener, ActionListener{
 		}
 		
 		//color picker grid
-		x = 600;
-		y =10;
+		x = 550;
+		y = 10;
 		for(int r = 0;r < 9;r ++){
 			for(int c = 0;c < 12;c ++){
 				
@@ -132,8 +202,6 @@ public class Screen extends JPanel implements MouseListener, ActionListener{
 					}
 				}
 				
-				
-				
 				//draw square
 				g.setColor( new Color(red,green,blue) );
 				g.fillRect(x,y,30,30);
@@ -142,36 +210,62 @@ public class Screen extends JPanel implements MouseListener, ActionListener{
 				x += 30;
 			}
 			y += 30;
-			x = 600;
+			x = 550;
+		}
+		//black to white
+		x = 910;
+		y = 10;
+		int red = 3;
+		int green = 3;
+		int blue = 3;
+		for(int r = 0;r < 9;r ++){
+			
+			if( r == 8 ){
+				g.setColor(Color.white);
+			} else if ( r == 0 ){
+				g.setColor(Color.black);
+			} else {
+				g.setColor( new Color(red,green,blue) );
+			}
+			g.fillRect(x,y,30,30);
+			g.setColor(Color.black);
+			g.drawRect(x,y,30,30);
+			y += 30;
+			
+			red += 28;
+			green += 28;
+			blue += 28;
+			
 		}
 		
 		//chosen color
 		g.setColor( new Color(redSet,greenSet,blueSet) );
-		g.fillRect(530,50,30,30);
+		g.fillRect(835,370,30,30);
 		g.setColor(Color.black);
-		g.drawRect(530,50,30,30);
-		g.drawString("Current Color",505,30);
+		g.drawRect(835,370,30,30);
+		g.setFont(font1);
+		g.drawString("Current Color",803,355);
+		
+		//choose tool
+		g.setColor(Color.black);
+		g.setFont(font1);
+		g.drawString("Current Tool: " + currentTool,575,350);
+		
+		
 		
 	}
 	
 	
 	 public void mousePressed(MouseEvent e) {
-		 
-		//draw on grid
-		int x = e.getX();
-		int y = e.getY();
-		if( x <= 490 && x >= 10 && y <= 490 && y >= 10 ){
-			int row = (y-10)/30;
-			int column = (x-10)/30;
-			grid[row][column].setColor(redSet,greenSet,blueSet);
-		}
 		
 		//set color
-		if( x >= 600 && x <= 960 && y >= 10 && y <= 280 ){
+		int x = e.getX();
+		int y = e.getY();
+		if( x >= 550 && x <= 910 && y >= 10 && y <= 280 ){
 		
 			int r = (y-10)/30;
-			int c = (x-600)/30;
-		
+			int c = (x-550)/30;
+			
 			if( c < 3 || c > 9 ){
 				if( r > 4 ){
 					redSet = 255;
@@ -235,14 +329,55 @@ public class Screen extends JPanel implements MouseListener, ActionListener{
 					blueSet = 0;
 				}
 			}
-		
+		} else if ( x >= 910 && x <= 940 && y >= 10 && y <= 280 ){
+			int r = (y-10)/30;
+			if( r == 8 ){
+				redSet = 255;
+				greenSet = 255;
+				blueSet = 255;
+			} else if ( r == 0 ){
+				redSet = 0;
+				greenSet = 0;
+				blueSet = 0;
+			} else {
+				redSet = 28 * r + 3;
+				greenSet = 28 * r + 3;
+				blueSet = 28 * r + 3;
+			}
 		}
 		
-        repaint();
+		//use tools
+		x = e.getX();
+		y = e.getY();
+		if( x <= 490 && x >= 10 && y <= 490 && y >= 10 ){
+			//store undo
+			if( store ){
+				storeUndo();
+			}
+			store = false;
+			
+			//fill or place color or eye dropper
+			int row = (y-10)/30;
+			int column = (x-10)/30;
+			if( currentTool.equals("Fill") ){
+				if( !compareColor(row,column,redSet,greenSet,blueSet) ){
+					fill(row, column);
+				}
+			} else if ( currentTool.equals("Eye Dropper") ){
+				eyeDropper(row,column);
+			} else {
+				grid[row][column].setColor(redSet,greenSet,blueSet);
+			}
+		}
+		
+		repaint();
+		
     }
  
  
-    public void mouseReleased(MouseEvent e) {}
+    public void mouseReleased(MouseEvent e) {
+    	store = true;
+    }
  
  
     public void mouseEntered(MouseEvent e) {}
@@ -255,7 +390,202 @@ public class Screen extends JPanel implements MouseListener, ActionListener{
 	
 	
 	public void actionPerformed(ActionEvent e){
+		if( e.getSource() == clearButton ){
+			for(int r = 0;r < grid.length;r ++){
+				for(int c = 0;c < grid[r].length;c ++){
+					grid[r][c].setColor(255,255,255);
+				}
+			}
+		} else if ( e.getSource() == undoButton ){
+			undo();
+		} else if ( e.getSource() == fillButton ){
+			currentTool = "Fill";
+		} else if ( e.getSource() == saveFileButton  ){
+			myImage();
+		} else if ( e.getSource() == drawButton ){
+			currentTool = "Draw";
+		} else if ( e.getSource() == eyeDropperButton ){
+			currentTool = "Eye Dropper";
+		}
 		repaint();
 	}
+	
+	
+	public void mouseDragged(MouseEvent e){
+		if( currentTool.equals("Draw") ){
+			int x = e.getX();
+			int y = e.getY();
+			if( x <= 490 && x >= 10 && y <= 490 && y >= 10 ){
+				mousePressed(e);
+			}
+		}
+		
+	}
+	
+	
+	public void mouseMoved(MouseEvent e){}
+	
+	
+	public void storeUndo(){
+		
+		//third undo
+		for(int r = 0;r < undoGrid3.length;r ++){
+			for(int c = 0;c < undoGrid3[r].length;c ++){
+				undoGrid3[r][c].setColor(undoGrid2[r][c].getRed(),undoGrid2[r][c].getGreen(),undoGrid2[r][c].getBlue());
+			}
+		}
+		
+		//second undo
+		for(int r = 0;r < undoGrid2.length;r ++){
+			for(int c = 0;c < undoGrid2[r].length;c ++){
+				undoGrid2[r][c].setColor(undoGrid1[r][c].getRed(),undoGrid1[r][c].getGreen(),undoGrid1[r][c].getBlue());
+			}
+		}
+		
+		//first undo
+		for(int r = 0;r < undoGrid1.length;r ++){
+			for(int c = 0;c < undoGrid1[r].length;c ++){
+				undoGrid1[r][c].setColor(grid[r][c].getRed(),grid[r][c].getGreen(),grid[r][c].getBlue());
+			}
+		}
+		
+	}
+	
+	
+	public void undo(){
+		
+		//set board
+		for(int r = 0;r < grid.length;r ++){
+			for(int c = 0;c < grid[r].length;c ++){
+				grid[r][c].setColor(undoGrid1[r][c].getRed(),undoGrid1[r][c].getGreen(),undoGrid1[r][c].getBlue());
+			}
+		}
+		
+		//move up second undo
+		for(int r = 0;r < undoGrid1.length;r ++){
+			for(int c = 0;c < undoGrid1[r].length;c ++){
+				undoGrid1[r][c].setColor(undoGrid2[r][c].getRed(),undoGrid2[r][c].getGreen(),undoGrid2[r][c].getBlue());
+			}
+		}
+		
+		//move up third undo
+		for(int r = 0;r < undoGrid2.length;r ++){
+			for(int c = 0;c < undoGrid2[r].length;c ++){
+				undoGrid2[r][c].setColor(undoGrid3[r][c].getRed(),undoGrid3[r][c].getGreen(),undoGrid3[r][c].getBlue());
+			}
+		}
+		
+		repaint();
+		
+	}
+	
+	public void fill(int row, int col){
+		grid[row][col].fillTrue();
+		int red1 = grid[row][col].getRed();
+		int green1 = grid[row][col].getGreen();
+		int blue1 = grid[row][col].getBlue();
+		grid[row][col].setColor(redSet,greenSet,blueSet);
+		
+		boolean keepFilling = true;
+		while( keepFilling ){
+			keepFilling = false;
+			//loop through every spot
+			for(int r = 0;r < grid.length;r ++){
+				for(int c = 0;c < grid[r].length;c ++){
+					//if one next to the spot is filled
+					if( r-1 >= 0 && r-1 <= 15 && compareColor(r,c,red1,green1,blue1) ){
+						if( grid[r-1][c].getFill() ){
+							grid[r][c].fillTrue();
+							grid[r][c].setColor(redSet,greenSet,blueSet);
+							keepFilling = true;
+						}
+					}
+					if ( r+1 >= 0 && r+1 <= 15 && compareColor(r,c,red1,green1,blue1) ){
+						if( grid[r+1][c].getFill() ){
+							grid[r][c].fillTrue();
+							grid[r][c].setColor(redSet,greenSet,blueSet);
+							keepFilling = true;
+						}
+					}
+					if ( c-1 >= 0 && c-1 <= 15 && compareColor(r,c,red1,green1,blue1) ){
+						if( grid[r][c-1].getFill() ){
+							grid[r][c].fillTrue();
+							grid[r][c].setColor(redSet,greenSet,blueSet);
+							keepFilling = true;
+						}
+					}
+					if ( c+1 >= 0 && c+1 <= 15 && compareColor(r,c,red1,green1,blue1) ){
+						if( grid[r][c+1].getFill() ){
+							grid[r][c].fillTrue();
+							grid[r][c].setColor(redSet,greenSet,blueSet);
+							keepFilling = true;
+						}
+					}
+				}
+			}
+		}
+		
+		for(int r = 0;r < grid.length;r ++){
+			for(int c = 0;c < grid[r].length;c ++){
+				grid[r][c].fillFalse();
+			}
+		}
+	}
+	
+	
+	public boolean compareColor(int row1, int col1, int red1, int green1, int blue1){
+		if( grid[row1][col1].getRed() != red1 ){
+			return false;
+		} else if ( grid[row1][col1].getGreen() != green1 ){
+			return false;
+		} else if ( grid[row1][col1].getBlue() != blue1 ){
+			return false;
+		}
+		return true;
+	}
+	
+	public void myImage(){
+		int width = 481;
+		int height = 480;
+		BufferedImage image = null;
+		File f = null;
+		
+		//read image file
+		try{
+			f = new File("PixelArt.png");
+			image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+			
+			Graphics2D g2d = image.createGraphics();
+			
+			int x = 0;
+			int y = 0;
+			for(int r = 0;r < grid.length;r ++){
+				for(int c = 0;c < grid[r].length;c ++){
+					grid[r][c].drawMe(g2d,x,y);
+					x += 30;
+				}
+				x = 0;
+				y += 30;
+			}	
+				
+			g2d.dispose();
+			
+			// Save as PNG
+       	 	File file = new File("myimage.png");
+        	ImageIO.write(image, "png", file);
+	
+		} catch (IOException e){
+			System.out.println("Error: " + e);
+		}
+		
+		
+	}
+	
+	public void eyeDropper(int row,int col){
+		redSet = grid[row][col].getRed();
+		greenSet = grid[row][col].getGreen();
+		blueSet = grid[row][col].getBlue();
+	}
+	
 	
 }
--- Square.java
+++ Square.java
@@ -4,11 +4,13 @@ import java.awt.Color;
 public class Square{
 
 	public int red, green, blue;
+	public boolean fill;
 	
 	public Square(){
 		red = 255;
 		green = 255;
 		blue = 255;
+		fill = false;
 	}
 	
 	public void drawMe(Graphics g, int x, int y){
@@ -22,6 +24,34 @@ public class Square{
 		this.red = red;
 		this.green = green;
 		this.blue = blue;
+	}
+	
+	public int getRed(){
+		return red;
+	}
+	
+	public int getGreen(){
+		return green;
+	}
+	
+	public int getBlue(){
+		return blue;
+	}
+	
+	public void print(){
+		System.out.println(red);
+	}
+	
+	public void fillTrue(){
+		fill = true;
+	}
+	
+	public void fillFalse(){
+		fill = false;
+	}
+	
+	public boolean getFill(){
+		return fill;
 	}
 
 }
